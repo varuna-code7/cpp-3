d6b1b72fb7fbf2bcf219f0a5fe4c2932194f5fbd
varuna-code7 <muthuvaruna4@gmail.com>
2025-08-02 20:28:53 +0530

    swap.cpp is added

--- swap.cpp
+++ swap.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    string x,y;
+    cout<<"enter x and y"<<endl;
+    cin>>x>>y;
+    x.swap(y);
+    cout<<"after swapping x"<<x;
+    cout<<"after swapping y"<<y;
+    return 0;
+}    
